6a6b635dc23ec87186226fb7687bbe17ec691199
Daniel Flyer <dflyer@princeton.edu>
2024-03-07 19:14:31 +0000

    updated unit tests

--- test_codes/hello_pthreads.c
+++ test_codes/hello_pthreads.c
@@ -5,21 +5,27 @@
 // Function to be executed by the thread
 void *printHello(void *threadId) {
     long tid = (long)threadId;
-    printf("Hello, World! from thread #%ld\n", tid);
+    fprintf(stderr,"Hello, World! from thread #%ld\n", tid);
     pthread_exit(NULL);
 }
 
-int main() {
-    int numThreads = 2; // Number of threads to create
+int main(int argc, char *argv[]) {
+    fprintf(stderr, "Hello Pthreads Beginning\n");
+    int numThreads;
+    if(argc < 2){
+        numThreads = 1; // Default to one threads
+    }else{
+        numThreads = atoi(argv[1]);
+    }
     pthread_t threads[numThreads];
     int rc;
     long t;
 
     for (t = 0; t < numThreads; t++) {
-        printf("Creating thread %ld\n", t);
+        fprintf(stderr,"Creating thread %ld\n", t);
         rc = pthread_create(&threads[t], NULL, printHello, (void *)t);
         if (rc) {
-            printf("Error: Unable to create thread %ld\n", t);
+            fprintf(stderr,"Error: Unable to create thread %ld\n", t);
             exit(-1);
         }
     }
@@ -29,7 +35,7 @@ int main() {
         pthread_join(threads[t], NULL);
     }
 
-    printf("All threads have completed.\n");
+    fprintf(stderr, "All threads have completed.\n");
 
     pthread_exit(NULL);
 }
--- test_codes/pthreads_lock.c
+++ test_codes/pthreads_lock.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define NUM_THREADS 4
 #define NUM_INCREMENTS 2
 
 int sharedVariable = 0;
@@ -18,39 +17,46 @@ void *incrementVariable(void *threadId) {
     pthread_exit(NULL);
 }
 
-int main() {
-    pthread_t threads[NUM_THREADS];
+int main(int argc, char *argv[]) {
+    fprintf(stderr, "Pthreads Lock Beginning\n");
+    int numThreads;
+    if(argc < 2){
+        numThreads = 1; // Default to one threads
+    }else{
+        numThreads = atoi(argv[1]);
+    }
+    pthread_t threads[numThreads];
     int rc;
     long t;
 
     // Initialize the mutex lock
     pthread_mutex_init(&lock, NULL);
 
-    for (t = 0; t < NUM_THREADS; t++) {
-        printf("Creating thread %ld\n", t);
+    for (t = 0; t < numThreads; t++) {
+        fprintf(stderr, "Creating thread %ld\n", t);
         rc = pthread_create(&threads[t], NULL, incrementVariable, (void *)t);
         fprintf(stderr, "%s", "Thread is created!\n");
         if (rc) {
-            printf("Error: Unable to create thread %ld\n", t);
+            fprintf(stderr, "Error: Unable to create thread %ld\n", t);
             exit(-1);
         }
-        printf("Increment done %ld\n", t);
+        fprintf(stderr, "Increment done %ld\n", t);
     }
 
     // Wait for all threads to complete
-    for (t = 0; t < NUM_THREADS; t++) {
-        printf("joing_thread %ld\n", t);
+    for (t = 0; t < numThreads; t++) {
+        fprintf(stderr, "joing_thread %ld\n", t);
         //pthread_join(threads[t], NULL);
-	    printf("Thread_joined %ld\n",t);
+	    fprintf(stderr, "Thread_joined %ld\n",t);
     }
 
-    printf("All threads have completed.\n");
-    printf("Final sharedVariable value: %d\n", sharedVariable);
+    fprintf(stderr, "All threads have completed.\n");
+    fprintf(stderr, "Final sharedVariable value: %d\n", sharedVariable);
 
     // Destroy the mutex lock
     pthread_mutex_destroy(&lock);
 
-    printf("MUTEX_LOCK_DESTROYED.\n");
+    fprintf(stderr, "MUTEX_LOCK_DESTROYED.\n");
 
     //pthread_exit(NULL);
     exit(0);
